6c5603379f5c05a59ef37a7115d83b03a8219e89
Park52 <wogus8660@gmail.com>
2019-05-23 03:31:22 +0900

    Heap And PriorityQueue
    Practice Heap And PriorityQueue

---
+++ PriorityQueue/PriorityQueue.h
@@ -0,0 +1,29 @@
+#ifndef PRIORITYQUEUE_H
+#define PRIORITYQUEUE_H
+
+#include <stdio.h>
+#include <memory.h>
+#include <stdlib.h>
+
+typedef struct tagPQNode
+{
+	int		Priority;
+	void*	Data;
+}PQNode;
+
+typedef struct tagPriorityQueue
+{
+	PQNode* Nodes;
+	int Capacity;
+	int UsedSize;
+}PriorityQueue;
+
+PriorityQueue*	PQ_Create(int InitialSize);
+void			PQ_Destroy(PriorityQueue* PQ);
+void			PQ_Enqueue(PriorityQueue* PQ, PQNode NewData);
+void			PQ_Dequeue(PriorityQueue* PQ, PQNode* Root);
+int				PQ_GetParent(int Index);
+int				PQ_GetLeftChild(int Index);
+void			PQ_SwapNodes(PriorityQueue* PQ, int Index1, int Index2);
+int				PQ_IsEmptry(PriorityQueue* PQ);
+#endif
